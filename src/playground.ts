1872961ca967967cc6b7b3bbf80b433af9f0bf50
Matt Pocock <mattpocockvoice@gmail.com>
2023-03-09 17:26:55 +0000

    NICE

--- src/playground.ts
+++ src/playground.ts
@@ -6,8 +6,8 @@
 interface UntypeableBase<TLevels extends readonly string[] = []> {
   addLevel: <TLevel extends string>() => UntypeableBase<[...TLevels, TLevel]>;
   router: () => UntypeableRouter<TLevels>;
-  input: <TInput>() => UntypeableInput<TInput, void>;
-  output: <TOutput>() => UntypeableOutput<void, TOutput>;
+  input: <TInput>() => UntypeableInput<TInput>;
+  output: <TOutput>() => UntypeableOutput<{}, TOutput>;
 }
 
 type Pop<L extends ReadonlyArray<any>> = L extends
@@ -47,7 +47,7 @@ type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
 
-interface UntypeableInput<TInput, TOutput> {
+interface UntypeableInput<TInput> {
   __type: "input";
   output: <TOutput>() => UntypeableOutput<TInput, TOutput>;
 }
@@ -68,20 +68,94 @@ interface UntypeableRouter<
     config?: TConfig,
   ) => UntypeableRouter<TLevels, Prettify<TRoutes & Record<TPath, TConfig>>>;
 
-  handler: () => <
-    TPath extends keyof TRoutes,
-    TArgsWithReturn extends RoutesToHandlerArgsAndReturn<
-      TRoutes[TPath]
-    > extends infer X extends any[]
-      ? X
-      : never,
-  >(
-    ...args: [
-      path: TPath,
-      ...args: TArgsWithReturn extends [...infer TArgs, any] ? TArgs : never,
-    ]
-  ) => TArgsWithReturn extends [...any[], infer TReturn] ? TReturn : never;
+  handler: () => UntypeableHandler<TLevels, TRoutes>;
+  merge: <TRoutes2 extends Record<string, any>>(
+    router: UntypeableRouter<TLevels, TRoutes2>,
+  ) => UntypeableRouter<TLevels, Prettify<TRoutes & TRoutes2>>;
 }
+
+type UntypeableHandler<
+  TLevels extends readonly string[] = [],
+  TRoutes extends Record<string, any> = {},
+> = TLevels["length"] extends 0
+  ? <T1 extends keyof TRoutes>(
+      path: T1,
+      ...args: ArgsFromConfig<T1 & string, TRoutes[T1]>
+    ) => TRoutes[T1] extends UntypeableOutput<any, infer TOutput>
+      ? Promise<TOutput>
+      : never
+  : TLevels["length"] extends 1
+  ? <T1 extends keyof TRoutes, T2 extends keyof TRoutes[T1]>(
+      path: T1,
+      firstLevel: T2,
+      ...args: ArgsFromConfig<T1 & string, TRoutes[T1][T2]>
+    ) => TRoutes[T1][T2] extends UntypeableOutput<any, infer TOutput>
+      ? Promise<TOutput>
+      : never
+  : TLevels["length"] extends 2
+  ? <
+      T1 extends keyof TRoutes,
+      T2 extends keyof TRoutes[T1],
+      T3 extends keyof TRoutes[T1][T2],
+    >(
+      path: T1,
+      firstLevel: T2,
+      secondLevel: T3,
+      ...args: ArgsFromConfig<T1 & string, TRoutes[T1][T2][T3]>
+    ) => TRoutes[T1][T2][T3] extends UntypeableOutput<any, infer TOutput>
+      ? Promise<TOutput>
+      : never
+  : TLevels["length"] extends 3
+  ? <
+      T1 extends keyof TRoutes,
+      T2 extends keyof TRoutes[T1],
+      T3 extends keyof TRoutes[T1][T2],
+      T4 extends keyof TRoutes[T1][T2][T3],
+    >(
+      path: T1,
+      firstLevel: T2,
+      secondLevel: T3,
+      thirdLevel: T4,
+      ...args: ArgsFromConfig<T1 & string, TRoutes[T1][T2][T3][T4]>
+    ) => TRoutes[T1][T2][T3][T4] extends UntypeableOutput<any, infer TOutput>
+      ? Promise<TOutput>
+      : never
+  : never;
+
+type ArgsFromConfig<
+  TPath extends string,
+  TConfig,
+  PathParams = ExtractPathParams<TPath>,
+> = TConfig extends UntypeableOutput<infer TInput, any>
+  ? TInput & PathParams extends Record<string, never>
+    ? []
+    : [input: Prettify<TInput & PathParams>]
+  : never;
+
+// interface UntypeableHandler<
+//   TLevels extends readonly string[] = [],
+//   TRoutes extends Record<string, any> = {},
+// > {
+//   <T1 extends keyof TRoutes>(
+//     path: T1,
+//     input: TRoutes[T1] extends UntypeableOutput<infer TInput, any>
+//       ? TInput
+//       : never,
+//   ): TRoutes[T1] extends UntypeableOutput<any, infer TOutput>
+//     ? Promise<TOutput>
+//     : never;
+//   <T1 extends keyof TRoutes, T2 extends keyof TRoutes[keyof TRoutes]>(
+//     path: T1,
+//     firstLevel: T2,
+//     input: TRoutes[T1][T2] extends UntypeableOutput<infer TInput, any>
+//       ? TInput extends void
+//         ? []
+//         : [input: TInput]
+//       : never,
+//   ): TRoutes[T1][T2] extends UntypeableOutput<any, infer TOutput>
+//     ? Promise<TOutput>
+//     : never;
+// }
 
 type StringArrayToObject<T extends readonly string[], TValue> = T extends [
   infer THead extends string,
@@ -105,7 +179,8 @@ const basicRouter = u1
   .router()
   .add("/user/:id", u1.output<User>())
   .add("/create-user", u1.input<{ name: string }>().output<User>())
-  .add("/user/:id/update", u1.input<{ name: string }>().output<User>());
+  .add("/user/:id/update", u1.input<{ name: string }>().output<User>())
+  .add("/delete-user/:id", u1.output<boolean>());
 
 const u = initUntypeable().addLevel<"GET" | "POST" | "PUT" | "DELETE">();
 
@@ -120,29 +195,65 @@ const userRouter = u
     POST: u.input<{ name: string }>().output<User>(),
   });
 
-type RoutesToHandlerArgsAndReturn<
-  Routes,
-  TArgs extends readonly any[] = [],
-> = Routes extends UntypeableOutput<infer TInput, infer TOutput>
-  ? TInput extends void
-    ? [...args: TArgs, output: TOutput]
-    : [...args: TArgs, input: TInput, output: TOutput]
-  : Routes extends Record<string, any>
-  ? {
-      [K in keyof Routes]: RoutesToHandlerArgsAndReturn<
-        Routes[K],
-        [...args: TArgs, arg: K]
-      >;
-    }[keyof Routes]
-  : [];
+const postRouter = u
+  .router()
+  .add("/post/:id", {
+    GET: u.output<User>(),
+    PUT: u.input<{ name: string; orgId: string }>().output<User>(),
+    DELETE: u.output<boolean>(),
+  })
+  .add("/post", {
+    POST: u.input<{ name: string }>().output<User>(),
+  });
 
-const handler = userRouter.handler();
+const handler = userRouter.merge(postRouter).handler();
 
 handler("/user/:id", "PUT", {
+  id: "awdawd",
+  name: "awdawd",
+  orgId: "awdawd",
+});
+
+handler("/user", "POST", {
+  id: "124123",
   name: "awdawd",
   orgId: "awdawd",
 });
 
 const basicHandler = basicRouter.handler();
 
-basicHandler("/user/:id");
+basicHandler("/create-user", {
+  name: "awdawd",
+});
+
+basicHandler("/user/:id", {
+  id: "awdawd",
+});
+
+basicHandler("/user/:id/update", {
+  id: "awdawd",
+  name: "awdawd",
+});
+
+basicHandler("/delete-user/:id", {
+  id: "124123",
+});
+
+const u3 = initUntypeable()
+  .addLevel<"WOW" | "WOW2">()
+  .addLevel<"AMAZING" | "EPIC">()
+  .addLevel<"MUST" | "GO" | "DEEPER">();
+
+const deepRouter = u3.router().add("/deep", {
+  WOW: {
+    AMAZING: {
+      DEEPER: u3.output<string>(),
+      GO: u3.output<number>(),
+      MUST: u3.output<string>(),
+    },
+  },
+});
+
+const deepHandler = deepRouter.handler();
+
+const result = deepHandler("/deep", "WOW", "AMAZING", "GO");
